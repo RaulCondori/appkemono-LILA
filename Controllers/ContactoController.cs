ad4a69f05344f88bfe6e6b077006b9aebc8e0ddf
raulcondori <raul_condori@usmp.pe>
2022-05-06 16:28:37 -0500

    INICIO

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using appcomics.Models;
+using appcomics.Data;
+namespace appcomics.Controllers
+{
+    public class ContactoController: Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public ContactoController(ApplicationDbContext context,
+            ILogger<ContactoController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contacto objContacto)
+        {
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            ViewData["Message"] = "Se registro el contacto";
+            return View("Index");
+        }
+
+
+    }
+}
